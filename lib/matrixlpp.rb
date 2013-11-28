0977da929292ea83594060bc70ae50d6650abe42
Javier Mena <alu0100454741@ull.edu.es>
2013-11-28 19:03:09 +0000

    Estructurando practica

--- lib/matrixlpp.rb
+++ lib/matrixlpp.rb
@@ -1,4 +1,12 @@
 require "matrixlpp/version"
+require "matrixlpp/matriz.rb"
+require "matrixlpp/matriz_densa.rb"
+require "matrixlpp/matriz_dispersa.rb"
+require "matrixlpp/matriz_operaciones.rb"
+
+require "matrixlpp/racional.rb"
+require "matrixlpp/gcd.rb"
+
 
 module Matrixlpp
   # Your code goes here...
