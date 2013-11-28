a5454456a3fcc2c2077cedb7c548875c1f849a18
Javier Mena <alu0100454741@ull.edu.es>
2013-11-28 21:52:12 +0000

    Cambios dentro de matrixlpp.rb

--- lib/matrixlpp.rb
+++ lib/matrixlpp.rb
@@ -1,13 +1,10 @@
 require "./lib/matrixlpp/version"
-require "./lib/matrixlpp/matriz.rb"
-require "./lib/matrixlpp/matriz_densa.rb"
-require "./lib/matrixlpp/matriz_dispersa.rb"
-require "./lib/matrixlpp/matriz_operaciones.rb"
-
-require "./lib/matrixlpp/racional.rb"
-require "./lib/matrixlpp/gcd.rb"
-
 
 module Matrixlpp
-  # Your code goes here...
+	require "./lib/matrixlpp/matriz.rb"
+	require "./lib/matrixlpp/matriz_densa.rb"
+	require "./lib/matrixlpp/matriz_dispersa.rb"
+	require "./lib/matrixlpp/matriz_operaciones.rb"
+	require "./lib/matrixlpp/racional.rb"
+	require "./lib/matrixlpp/gcd.rb"
 end
